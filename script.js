ebe093a4c672b9c49099e278db8a8e23ac8119f0
mohammed aban <mohammedaban777@gmail.com>
2024-08-21 22:47:48 +0530

    update related to equals_sign
    - when clicked on equals sign, only then it will be visible.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const result = document.getElementById("result");
 const calculation = document.querySelector(".calculation")
+
 function restrictInput(input) {
     // Allow only digits 1-9 and operators +-*%/.
     const validChars = /[^123456789\.\/\+\-\*\%]/g;
@@ -12,12 +13,37 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-function final_result() {
+function not_equals() {
+    const equals_logo = document.getElementById("hidden");
+    equals_logo.classList.add("hidden_class");
+}
+
+function if_equals(){
+    const equals_logo = document.getElementById("hidden");
+    equals_logo.classList.remove("hidden_class");
+}
+
+function add(input) {
+    result.value += input;
+    not_equals();
+}
+
+function backspace() {
+    result.value = result.value.toString().slice(0, -1);
+    not_equals();
+    if (result.value == "") {
+        calculation.value = "";
+    }
+}
+
+function final_result(){
     calculation.value = result.value;
+    if_equals()
     result.value = eval(result.value);
 }
 
 function delete_everything(){
+    not_equals();
     result.value = "";
     calculation.value = "";
 }
