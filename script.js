78570fb20e6332c6a3554fd152f1ee0d6946d806
mohammed aban <mohammedaban777@gmail.com>
2024-10-20 20:02:45 +0530

    Added 0 as a keyboad input.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const calculation = document.querySelector(".calculation")
 
 function restrictInput(input) {
     // Allow only digits 1-9 and operators +-*%/.
-    const validChars = /[^123456789\.\/\+\-\*\%]/g;
+    const validChars = /[^1234567890\.\/\+\-\*\%]/g;
     input.value = input.value.replace(validChars, '');
 }
 document.addEventListener('keydown', function(event) {
